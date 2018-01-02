f9190b5fb0a679ff1ce6593c917aca272955ff9c
CloudXue <eee_9360@qq.com>
2018-01-02 22:01:08 +0800

    think in java 书中第七章课后练习提交

---
+++ src/main/java/practice/sevenexternal/Practice15External.java
@@ -0,0 +1,14 @@
+package practice.sevenexternal;
+
+/**
+ * 功能说明: <br>
+ * 系统版本: 1.0 <br>
+ * 开发人员: xuesl
+ * 开发时间: 2018/1/2<br>
+ * <br>
+ */
+public class Practice15External {
+    protected void test(){
+        System.out.println("test");
+    }
+}
