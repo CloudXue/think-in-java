24b16f734cab1d1e07c727cce2b7909ddae71bd6
CloudXue <eee_9360@qq.com>
2018-01-02 11:01:37 +0800

    think in java 书中第六章源码提交

--- src/main/java/access/FullQualification.java
+++ src/main/java/access/FullQualification.java
@@ -1,7 +1,9 @@
 package access;//: access/FullQualification.java
 
+import java.util.ArrayList;
+
 public class FullQualification {
   public static void main(String[] args) {
-    java.util.ArrayList list = new java.util.ArrayList();
+    ArrayList list = new ArrayList();
   }
 } ///:~
